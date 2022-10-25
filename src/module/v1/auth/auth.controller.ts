b17b68e2dcd1cb69552b392c703a9a8ef5469368
ĐỖ ANH QUỐC <45909444+quoc-it@users.noreply.github.com>
2022-10-25 16:38:01 +0700

    Feature/logout (#4)
    * feature login
    
    * update code
    
    * commit code
    
    * commit code
    
    * update code
    
    * commit code
    
    * commit code
    
    * update code
    
    * fix-bugs
    
    * update gitignore
    
    * commit code
    
    * update code
    
    * commit code
    
    * update code

--- src/module/v1/auth/auth.controller.ts
+++ src/module/v1/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, Req, Res } from '@nestjs/common'
+import { Body, Controller, Logger, Post, Req, Res } from '@nestjs/common'
 import { ApiResponse, ApiTags } from '@nestjs/swagger'
 import { Request, Response } from 'express'
 import * as COOKIE from '../../../utils/constants/cookie'
@@ -10,24 +10,25 @@ import { AuthService } from './auth.service'
   version: '1',
 })
 export class AuthController {
+  private readonly logger = new Logger(AuthService.name)
   constructor(private authService: AuthService) {}
 
   @Post('login')
   @ApiResponse({ status: 200, description: 'OK' })
   @ApiResponse({ status: 403, description: 'Forbidden.' })
-  async login(@Body() body: LoginDTO, @Res() response: Response) {
+  async login(@Body() body: LoginDTO, @Req() request: Request, @Res() response: Response) {
     try {
+      this.logger.log(`${request.hostname}- Login`)
       const { idToken } = body
-
       const [accessToken, refreshToken] = await this.authService.login(idToken)
-
       response.cookie(COOKIE.KEYS.REFRESH_TOKEN, refreshToken, {
         httpOnly: true,
       })
       const result = { accessToken }
       return response.send(result)
     } catch (e) {
-      return response.status(403).send()
+      this.logger.error(`${request.hostname}- Login: ${e}`)
+      return response.status(403).end()
     }
   }
   @Post('refreshToken')
@@ -35,12 +36,26 @@ export class AuthController {
   @ApiResponse({ status: 403, description: 'Forbidden.' })
   refreshToken(@Req() request: Request, @Res() response: Response) {
     try {
+      this.logger.log(`${request.hostname}- RefreshToken`)
       const refreshToken = request.cookies[COOKIE.KEYS.REFRESH_TOKEN]
       const accessToken = this.authService.refreshToken(refreshToken)
       const result = { accessToken }
       return response.status(200).send(result)
     } catch (e) {
-      return response.status(403).send()
+      this.logger.error(`${request.hostname}- RefreshToken: ${e}`)
+      return response.status(403).end()
+    }
+  }
+  @Post('logout')
+  @ApiResponse({ status: 200, description: 'OK' })
+  logout(@Req() request: Request, @Res() response: Response) {
+    try {
+      this.logger.log(`${request.hostname}- Logout`)
+      response.clearCookie(COOKIE.KEYS.REFRESH_TOKEN)
+      return response.status(200).end()
+    } catch (e) {
+      this.logger.error(`${request.hostname}- Logout: ${e}`)
+      return response.status(400).end()
     }
   }
 }
